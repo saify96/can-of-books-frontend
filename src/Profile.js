d0457526fa286085220c9a131c068744bc41fb90
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-06-20 18:26:31 +0300

    return the isloding and change the profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
-import { withAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-export class Profile extends Component {
-    render() {
-      let  userName= this.props.auth0.user.name;
-      let  userEmail= this.props.auth0.user.email;
-      let  userPicture= this.props.auth0.user.picture;
-    return (
-            <div>
-                <h2>{userName}</h2>
-                <p>{userEmail}</p>
-                <img src={userPicture} alt={userName} />
-            </div>
-        )
-    }
-}
-export default withAuth0(Profile)
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
+
+export default Profile;
